e43d9c866d5d17039f05dc01e2a364cfc3da0c58
JSPRO-11 <js-pro-11@JSPRO-11s-MacBook-Pro.local>
2022-03-29 17:32:47 +0530

    final update

--- pages/contact-us/index.vue
+++ pages/contact-us/index.vue
@@ -16,7 +16,7 @@
                     </div>
                     <div class="office-info-text">
                       <h2>Address</h2>
-                      <p>7 Green Lake Street Crawfordsville, IN 47933</p>
+                      <p>#1, 1st Floor, Vasavi Complex, 147, Seshadripuram Main Road, Bangalore - 560020</p>
                     </div>
                   </div>
                 </div>
@@ -29,8 +29,8 @@
                     </div>
                     <div class="office-info-text">
                       <h2>Email Us</h2>
-                      <p>anur@gmail.com</p>
-                      <p>helloyou@gmail.com</p>
+                      <p>info@acegiftingsolutions.com</p>
+                      <p>ritika@acegiftingsolutions.com</p>
                     </div>
                   </div>
                 </div>
@@ -43,8 +43,8 @@
                     </div>
                     <div class="office-info-text">
                       <h2>Call Now</h2>
-                      <p>+1 800 123 456 789</p>
-                      <p>+1 800 123 654 987</p>
+                      <p>+91 81510 56671</p>
+                      <p>+91 80959 56671</p>
                     </div>
                   </div>
                 </div>
@@ -53,8 +53,7 @@
             <div class="wpo-contact-title">
               <h2>Have Any Question?</h2>
               <p>
-                It is a long established fact that a reader will be distracted
-                content of a page when looking.
+                If you have any queries about our product range or shipping, get in touch and we will assist you gladly.
               </p>
             </div>
             <div class="wpo-contact-form-area">
@@ -127,7 +126,7 @@
       <section class="wpo-contact-map-section">
         <div class="wpo-contact-map">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.9147703055!2d-74.11976314309273!3d40.69740344223377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbd!4v1547528325671"
+             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.651740058251!2d77.57576581482226!3d12.994112490841115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17469f168e05%3A0xdce4c7510fe2c2a7!2sAce%20Gifting%20Solutions!5e0!3m2!1sen!2sin!4v1648555261527!5m2!1sen!2sin" 
           ></iframe>
         </div>
       </section>
@@ -255,3 +254,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.wpo-contact-pg-section .office-info .office-info-item {
+    min-height: 340px;
+}
+</style>
