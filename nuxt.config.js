a66f0a2cd9fdcf71d5acf2ab9bee0d873885a6b9
root <root@ip-172-31-35-137.ap-south-1.compute.internal>
2025-06-10 15:30:42 +0000

    new

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,7 +96,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://localhost:8080/api/',
-    baseURL: 'https://www.acegiftingsolutions.com/api/',
+    baseURL: process.env.NUXT_PUBLIC_API_URL,
     credentials: true
   },
 
